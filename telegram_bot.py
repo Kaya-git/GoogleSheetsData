4b8d4bfe1163d8604b781941302da791fdabc68d
Kaya <ezjell@yandex.ru>
2023-12-11 15:05:00 +0300

    fix/text format

--- telegram_bot.py
+++ telegram_bot.py
@@ -15,7 +15,7 @@ async def send_message(chat_id: int = CHAT_ID):
 
     print("start")
     print(chat_id)
-    
+
     while True:
 
         new_cell = pull_new_cells()
@@ -39,34 +39,26 @@ async def send_message(chat_id: int = CHAT_ID):
         # Sell
         if new_cell[0] == "SELL":
             count_sell += 1
-            message_sell_text = f"""
-            {count_sell}
-            Операция: {new_cell[0]},
-            Сумма: {new_cell[1]},
-            Прибыль: {new_cell[2]},
-            Валюта: {new_cell[3]},
-            Курс: {new_cell[4]}
-            """
+            message_sell_text = f"""{count_sell}. {new_cell[1]} ({new_cell[2]}) {new_cell[3]}"""
 
             try:
                 message_sell
                 try:
                     message_edit_sell
-                    message_new = f"""
-                    {message_edit_sell.text},
-                    {message_sell_text}
-                    """
+                    message_new = (
+                        f"{message_edit_sell.text},\n{message_sell_text}"
+                    )
+
                     message_edit_sell = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_sell.message_id,
                         text=message_new
                     )
                 except NameError:
-                    message_new = f"""
-                    {message_sell.text},
-                    {message_sell_text}
+                    message_new = (
+                        f"{message_sell.text},\n{message_sell_text}"
+                    )
 
-                    """
                     message_edit_sell = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_sell.message_id,
@@ -82,33 +74,22 @@ async def send_message(chat_id: int = CHAT_ID):
         if new_cell[0] == "BUY":
             print("Покупка")
 
-            message_buy_text = f"""
-            #ОТКУП
-            Сумма RUB: -{new_cell[6]},
-            Сумма: +{new_cell[1]},
-            Валюта: {new_cell[3]},
-            Курс: {new_cell[4]}
-            """
+            message_buy_text = f"-{new_cell[6]}\n+{new_cell[1]} {new_cell[3]}\nКурс {new_cell[4]}"
 
             try:
                 message_buy
                 try:
                     message_edit_buy
-                    message_new = f"""
-                    {message_edit_buy.text},
-                    {message_buy_text}
-                    """
+                    message_new = f"{message_edit_buy.text},\n\n{message_buy_text}"
+
                     message_edit_buy = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_buy.message_id,
                         text=message_new
                     )
                 except NameError:
-                    message_new = f"""
-                    {message_buy.text},
-                    {message_buy_text}
+                    message_new = f"{message_buy.text},\n\n{message_buy_text}"
 
-                    """
                     message_edit_buy = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_buy.message_id,
@@ -117,38 +98,29 @@ async def send_message(chat_id: int = CHAT_ID):
             except NameError:
                 message_buy = await bot.send_message(
                     chat_id,
-                    text=message_buy_text
+                    text=f"#ОТКУП\n{message_buy_text}"
                 )
 
         # Расход
-        if new_cell[0] == "Расход":
+        if new_cell[0] == "РАСХОД":
             print("расход")
 
-            message_diff_text = f"""
-            #РАСХОД
-            Сумма RUB: -{new_cell[6]},
-            Комментарий: {new_cell[7]}
-            """
+            message_diff_text = f"-{new_cell[6]} {new_cell[7]}"
 
             try:
                 message_diff
                 try:
                     message_edit_diff
-                    message_new = f"""
-                    {message_edit_diff.text},
-                    {message_diff_text}
-                    """
+                    message_new = f"{message_edit_diff.text}\n{message_diff_text}"
+
                     message_edit_diff = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_diff.message_id,
                         text=message_new
                     )
                 except NameError:
-                    message_new = f"""
-                    {message_diff.text},
-                    {message_diff_text}
+                    message_new = f"{message_diff.text}\n{message_diff_text}"
 
-                    """
                     message_edit_diff = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_diff.message_id,
@@ -157,40 +129,29 @@ async def send_message(chat_id: int = CHAT_ID):
             except NameError:
                 message_diff = await bot.send_message(
                     chat_id,
-                    text=message_diff_text
+                    text=f"#РАСХОД\n{message_diff_text}"
                 )
 
         # Закуп
-        if new_cell[0] == "Закуп":
+        if new_cell[0] == "ЗАКУП":
             print("Закуп")
 
-            message_rebuy_text = f"""
-            #ЗАКУП
-            Сумма RUB: -{new_cell[6]},
-            Сумма: +{new_cell[1]},
-            Валюта: {new_cell[3]},
-            Курс: {new_cell[4]}
-            """
+            message_rebuy_text = f"-{new_cell[6]}\n+{new_cell[1]} {new_cell[3]}\nКурс {new_cell[4]}"
 
             try:
                 message_rebuy
                 try:
                     message_edit_rebuy
-                    message_new = f"""
-                    {message_edit_rebuy.text},
-                    {message_rebuy_text}
-                    """
+                    message_new = f"{message_edit_rebuy.text},\n\n{message_rebuy_text}"
+
                     message_edit_rebuy = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_rebuy.message_id,
                         text=message_new
                     )
                 except NameError:
-                    message_new = f"""
-                    {message_rebuy.text},
-                    {message_rebuy_text}
+                    message_new = f"{message_rebuy.text},\n\n{message_rebuy_text}"
 
-                    """
                     message_edit_rebuy = await bot.edit_message_text(
                         chat_id=chat_id,
                         message_id=message_rebuy.message_id,
@@ -199,7 +160,7 @@ async def send_message(chat_id: int = CHAT_ID):
             except NameError:
                 message_rebuy = await bot.send_message(
                     chat_id,
-                    text=message_rebuy_text
+                    text=f"#ЗАКУП\n{message_rebuy_text}"
                 )
 
 
